ba9cac13d0f86cdf02f3b2be98cb7ec3ce0a4545
Anton Prokopyev <inpokon@gmail.com>
2019-05-06 20:04:43 +0800

    #add: db.json

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {EmojiService} from "./service/emoji.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'test-emoji';
+
+  constructor(private emojiService: EmojiService) {}
+
+  ngOnInit() {
+    this.emojiService
+        .getEmoji()
+        .subscribe((response) => {
+          console.log(response);
+        });
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import {HttpClientModule} from "@angular/common/http";
+import {EmojiService} from "./service/emoji.service";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [EmojiService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
