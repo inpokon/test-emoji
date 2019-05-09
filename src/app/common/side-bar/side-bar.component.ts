65badbc69460facb791a9da305e7001b6fa27a93
Anton Prokopyev <inpokon@gmail.com>
2019-05-09 19:22:31 +0800

    #fix: работа с сервером

--- src/app/common/side-bar/side-bar.component.ts
+++ src/app/common/side-bar/side-bar.component.ts
@@ -7,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SideBarComponent implements OnInit {
 
+  links: Array<any> = [
+    {name: "Избранные", link: 'chosen'},
+    {name: "Удаленные", link: 'deleted'}
+  ];
+
   constructor() { }
 
   ngOnInit() {
